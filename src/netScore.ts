54478a2e4e8b949cf19db18ac5db9cceb9728146
Starnesc5780 <connordstarnes@gmail.com>
2024-09-20 16:55:58 -0400

    update netscore logic (not done)

--- src/netScore.ts
+++ src/netScore.ts
@@ -1,44 +1,77 @@
-//Main Function File
+import {handleURL} from './handleURL'
+import {checkSections} from './RampUp_Metric'
+import {getCorrectness} from './correctness'
+import {getBusFactor} from './busFactor'
+import {getResponsiveMaintainer} from './responsiveMaintainer'
+// import {getLicense} from './license'
+import * as fs from 'fs';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
-//Handle Input URL File
-
-//Read urls in loop
-
-//Per URL Operations (within loop)
-//1. get github repo name and owner (strings)
-//      -NOTE: this function will likely need to be async for handleURL.ts
-//2. pass repo name and owner into metric scoring functions
-//      -only the functions that will use the Github Rest/GraphQL API
-//3. metric scoring will return values
-//4. calculate net score of package
-//5. print output (to CLI)
-
-import {handleURL} from './handleURL' //CONFIRMED NECESSARY
-import {getBusFactor} from './busFactor' //CONFIRMED NECESSARY
-import * as fs from 'fs' //CONFIRMED NECESSARY
-async function main() {
-    const GITHUB_TOKEN = `YOUR GITHUB TOKEN HERE`; //enter your own github fine-grained personal access token here
+export async function RunProject() {
+    const TOKEN: string = process.env.GITHUB_TOKEN || '';
     const f_path = './Sample Url File.txt';
     const f_file = fs.readFileSync(f_path, 'utf-8');
     const lines: string[] = f_file.split(/\r?\n/);
+
+    let url_batch: string[] = [];
     for (const line of lines) { //for each URL in sample file
-        const repoinfo = await handleURL(line);
+        url_batch.push(line);
+        if (url_batch.length === 5) {
+            await processBatch(url_batch, TOKEN);
+        }
+    }
+}
+
+export async function processBatch(url_batch: string[], TOKEN: string) {
+    for (const url of url_batch) {
+        const repoinfo = await handleURL(url);
         if (!repoinfo) {
             console.error ("Error: URL not compatible. Please enter github.com or npmjs.com URL\n");
             return;
         }
         else {
-            //Logic for using repoinfo to get bus factor data
+            //Logic for using repoinfo to calculate net score
             const owner = repoinfo['owner'];
             const repo = repoinfo['repo'];
+            
+            const netScoreStart = Date.now();
+
+            //rampup call
+            // const rampUp = await getRampUp(owner, repo, TOKEN);
+            const rampUpEnd = Date.now();
+
+            const correctness = await getCorrectness(owner, repo, TOKEN);
+            const correctnessEnd = Date.now();
+
+            const busFactor = await getBusFactor(owner, repo, TOKEN);
+            const busFactorEnd = Date.now();
+
+            //responsiveMaintainer call
+            const responsiveMaintainerEnd = Date.now();
+
+            //license call
+            const licenseEnd = Date.now();
+
+            //net score calculation
+            let rampUp = 0;//for testing only
+            let responsiveMaintainer = 0;//for testing only
+            let license = 1;//for testing only
+            const netScore = (0.20*rampUp + 0.30*correctness + 0.20*busFactor + 0.30*responsiveMaintainer) * license;
+            const netScoreEnd = Date.now();
+
+            //latency calculations (in seconds)
+            const netScoreLatency = ((netScoreEnd - netScoreStart)/1000).toFixed(3);
+            const rampUpLatency = ((rampUpEnd - netScoreStart)/1000).toFixed(3);
+            const correctnessLatency = ((correctnessEnd - rampUpEnd)/1000).toFixed(3);
+            const busFactorLatency = ((busFactorEnd - correctnessEnd)/1000).toFixed(3);
+            const responsiveMaintainerLatency = ((responsiveMaintainerEnd - busFactorEnd)/1000).toFixed(3);
+            const licenseLatency = ((licenseEnd - responsiveMaintainerEnd)/1000).toFixed(3);
+
+            //print statements for testing
             console.log('Repo: ', repo, ' Owner: ', owner); //print output for testing purposes
-            const busFactor = await getBusFactor(owner, repo, GITHUB_TOKEN);
-            if (!busFactor) {
-                console.error ("Error: Could not get number of contributors\n");
-                return;
-            }
+            //add more for showing rest of metrics
             console.log('Has bus factor of ', busFactor.toFixed(2));
         }
     }
 }
-main();
