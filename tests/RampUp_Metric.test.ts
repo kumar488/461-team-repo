1d6e259f159f913d38a2d1e6fb7004f9d5b87e3d
AlejandroRodri726 <142841426+AlejandroRodri726@users.noreply.github.com>
2024-09-22 17:10:26 -0400

    Final RampUp_Metric.test.ts
    Vinal version of the test cases for the RampUp Metric

--- tests/RampUp_Metric.test.ts
+++ tests/RampUp_Metric.test.ts
@@ -5,42 +5,22 @@ import { test_RampUp, getRampUp } from '../src/RampUp_Metric';
 describe('Calculate RampUp Score', () => {
     jest.setTimeout(360000); //set timeout to 6 minutes
 
-
-    it('test_RampUp: Should return score of 0, bad README.md', async () => {
-        const url0 = "https://github.com/microsoft/TypeScript";
+    it('test_RampUp: Should return score 0.12, bad README', async () => {
+        const url0 = "https://github.com/lodash/lodash";
         var RU_score0 = await test_RampUp(url0);
         logger.info(`Ramp up score for ${url0}: ${RU_score0}`);
-        expect(RU_score0).toBe(0);
+        expect(RU_score0).toBe(0.12);
     }); 
-    it('getRampUp: Should return score of 0, bad README.md', async () => {
-        const url3 = "https://github.com/microsoft/TypeScript";
+    it('getRampUp: Should return score 0.12, bad README', async () => {
+        const url3 = "https://github.com/lodash/lodash";
         var RU_score3 = await getRampUp("cloudinary", url3, "Hi");
         logger.info(`Ramp up score for ${url3}: ${RU_score3}`);
-        expect(RU_score3).toBe(0);
+        expect(RU_score3).toBe(0.12);
     }); 
-    it('Should return score', async () => {
+    it('Should return score 0.35', async () => {
         const url2 = "https://github.com/cloudinary/cloudinary_npm";
         var RU_score2 = await test_RampUp(url2);
         logger.info(`Ramp up score for ${url2}: ${RU_score2}`);
         expect(RU_score2).toBe(0.35);
     }); 
-    // it('Should return score of 0, invalid url', async () => {
-    //     const url1 = "https://github.com/";
-    //     var RU_score1 = await test_RampUp(url1);
-    //     logger.info(`Ramp up score for ${url1}: ${RU_score1}`);
-    //     expect(RU_score1).toBe(0);
-    // }); 
-    
-    // it('getRampUp, Should return score of 0, bad README.md', async () => {
-    //     const url3 = "https://github.com/microsoft/TypeScript";
-    //     var RU_score3 = await getRampUp("cloudinary", url3, "Hi");
-    //     logger.info(`Ramp up score for ${url3}: ${RU_score3}`);
-    //     expect(RU_score3).toBe(0);
-    // }); 
-    // it('Should return score of 0, no README.md', async () => {
-    //     const url4 = "https://github.com/octocat/Hello-World/";
-    //     var RU_score4 = await test_RampUp(url4);
-    //     logger.info(`Ramp up score for ${url4}: ${RU_score4}`);
-    //     expect(RU_score4).toBe(0);
-    // }); 
 });
